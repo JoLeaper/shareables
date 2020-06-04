9eae5fe6512d5eb3c2096e524c2db93929421e68
JoLeaper <joleap123@gmail.com>
2020-06-04 13:46:41 -0700

    wrote out routes in comments

---
+++ index.js
@@ -0,0 +1,16 @@
+/* 
+app.post('/new-post', async(req, res) => {
+});
+
+app.get('/get-one-post', async(req, res) => {
+});
+
+app.get('/get-all-posts', async(req, res) => {
+});
+
+app.patch('/update-post', async(req, res) => {
+});
+
+app.delete('/delete-post', async(req, res) => {
+});
+*/
